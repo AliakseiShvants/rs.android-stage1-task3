380b652878ead6d5bbfee1ccc1ef69260cfb8be9
abovshevich <anna.bovshevich@artezio.com>
2020-05-02 23:41:35 +0300

    feat: implement array calculator

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,58 @@
 package subtask3
 
+import kotlin.math.absoluteValue
+
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        val numbers = getArrOfSortedNumberOrSkip(itemsFromArray)
+
+        if (numbers.isEmpty()) {
+            return 0
+        }
+
+        if (numberOfItems >= numbers.size) {
+            return mulAll(numbers)
+        }
+
+        var negativeCount = numbers.take(numberOfItems).filter { it < 0 }.count()
+
+        while (negativeCount % 2 == 1 && numbers.size > numberOfItems) {
+            val element = numbers.last { it < 0 }
+
+            numbers.removeAt(numbers.indexOf(element))
+            negativeCount = numbers.filter { it < 0 }.count()
+        }
+
+        var result = numbers[0]
+
+        for (x in 1 until numberOfItems) {
+            result *= numbers[x]
+        }
+
+        return result
+    }
+
+    private fun getArrOfSortedNumberOrSkip(itemsFromArray: Array<Any>): MutableList<Int> {
+        var result = mutableListOf<Int>()
+
+        itemsFromArray.forEach {
+            if (it is Int) result.add(it)
+        }
+
+        result.sortBy { it.absoluteValue }
+        result.reverse()
+
+        return result
+    }
+
+    private fun mulAll(numbers: List<Int>): Int {
+        var result = numbers[0]
+
+        for (x in 1 until numbers.size) {
+            result *= numbers[x]
+        }
+
+        return result
     }
 }
