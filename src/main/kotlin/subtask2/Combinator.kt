ad05666b8032fc6ae600f2ed19d2a2518e306e77
abovshevich <anna.bovshevich@artezio.com>
2020-05-03 09:15:41 +0300

    feat: implement combinator

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,25 @@ package subtask2
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val value = fuctorial(array[1]) / array[0]
+
+        for (x in 1 until array[1]) {
+            val result = fuctorial(x) * fuctorial(array[1] - x)
+
+            if (result == value) return x
+        }
+
+        return null
+    }
+
+    private fun fuctorial(number: Int): Long {
+        var result = 1L
+
+        for (x in 1..number) {
+            result *= x
+        }
+
+        return result
     }
 }
