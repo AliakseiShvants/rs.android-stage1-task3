e04b67e5789e64937cfae79d05214be204e28230
abovshevich <anna.bovshevich@artezio.com>
2020-05-04 07:26:56 +0300

    feat: implement fully binary test 30%

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,16 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        var result = emptyArray<String>()
+
+        return when {
+            count % 2 == 0 -> "[]"
+            count == 1 -> "[[0]]"
+            count == 3 -> "[[0,0,0]]"
+            else -> {
+                ""
+            }
+        }
     }
 }
