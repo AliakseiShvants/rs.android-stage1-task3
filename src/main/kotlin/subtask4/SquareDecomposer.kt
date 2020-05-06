e826bf77ccfd7886420b55fcee3a71f64e71b856
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-05-06 08:13:21 +0300

    feat: implement square decompose not fully

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,5 +1,8 @@
 package subtask4
 
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     fun decomposeNumber(number: Int): Array<Int>? {
@@ -7,19 +10,34 @@ class SquareDecomposer {
             return null
         }
 
-        val value = number * number
         var result = mutableListOf<Int>()
-
-//        while (va)
-
-
         result.add(number - 1)
 
+        val diff0: Double = (2 * number - 1).toDouble()
+        val start = floor(sqrt(diff0)).toInt()
+
+        for (x in start downTo 1) {
+            var tempList = mutableListOf<Int>()
+            var diff: Double = diff0 - x * x
+            tempList.add(x)
 
 
+            while (diff > 0) {
+                val item = floor(sqrt(diff)).toInt()
+                tempList.add(item)
+                diff -= item * item
+            }
 
+            if (isIncrease(tempList)) {
+                result.addAll(tempList)
+                result.sort()
 
+                return result.toTypedArray()
+            }
+        }
 
         return null
     }
+
+    private fun isIncrease(tempList: MutableList<Int>) = tempList.size == tempList.toSet().size
 }
